66631e5bcf6505220c633c9c869f5584a8c27bc6
Tomas Bauer <bauer@drosera.cz>
2012-03-14 13:26:30 +0100

    Instalace DoctrineMigrations

--- app/autoload.php
+++ app/autoload.php
@@ -5,18 +5,19 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
-    'Sensio'           => __DIR__.'/../vendor/bundles',
-    'JMS'              => __DIR__.'/../vendor/bundles',
-    'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
-    'Monolog'          => __DIR__.'/../vendor/monolog/src',
-    'Assetic'          => __DIR__.'/../vendor/assetic/src',
-    'Metadata'         => __DIR__.'/../vendor/metadata/src',
-    'CG'               => __DIR__.'/../vendor/cg-library/src',
-    'Stof'             => __DIR__.'/../vendor/bundles',
-    'Gedmo'            => __DIR__.'/../vendor/gedmo-doctrine-extensions/lib',
+    'Symfony'                       => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
+    'Sensio'                        => __DIR__.'/../vendor/bundles',
+    'JMS'                           => __DIR__.'/../vendor/bundles',
+    'Doctrine\\Common'              => __DIR__.'/../vendor/doctrine-common/lib',
+    'Doctrine\\DBAL\\Migrations'    => __DIR__.'/../vendor/doctrine-migrations/lib',
+    'Doctrine\\DBAL'                => __DIR__.'/../vendor/doctrine-dbal/lib',
+    'Doctrine'                      => __DIR__.'/../vendor/doctrine/lib',
+    'Monolog'                       => __DIR__.'/../vendor/monolog/src',
+    'Assetic'                       => __DIR__.'/../vendor/assetic/src',
+    'Metadata'                      => __DIR__.'/../vendor/metadata/src',
+    'CG'                            => __DIR__.'/../vendor/cg-library/src',
+    'Stof'                          => __DIR__.'/../vendor/bundles',
+    'Gedmo'                         => __DIR__.'/../vendor/gedmo-doctrine-extensions/lib',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
